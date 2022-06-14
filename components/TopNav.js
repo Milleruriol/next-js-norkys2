ccd72ab50f8f16ecf8eead1376ba26e8e6010220
Miller Uriol <milleruriol@gmail.com>
2022-06-14 00:28:45 -0400

    Changes in the Top Nav

--- components/TopNav.js
+++ components/TopNav.js
@@ -9,8 +9,8 @@ function TopNav() {
     <p className='pl-4 text-red-800 font-semibold'>Call Now</p>
     </div>
       <div className='p-4 flex align-center text-red-800' >
-      <button class="navbar-burger flex items-center text-blue-600 p-3">
-				<svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <button className="navbar-burger flex items-center text-blue-600 p-3">
+				<svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 					<title>Mobile menu</title>
 					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
 				</svg>
